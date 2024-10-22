bf3a5435bea2b40a12e615328ecb9f78a01af5e6
Serhii Prysiazhnyi <serge.prysiazhnyi@gmail.com>
2024-10-22 16:36:35 +0200

    add unit tests for App component with routing and authentication

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,47 @@
 import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 
 import App from './App'
+import * as storage from '../src/utils/storage'
 
 describe('App', () => {
-  test('should render App component', () => {
-    render(<App />)
+  afterEach(() => {
+    vi.clearAllMocks()
+  })
 
-    expect(screen.getByText(/app/i)).toBeInTheDocument()
+  const renderComponent = (initialRoute: string = '/') => {
+    render(
+      <MemoryRouter initialEntries={[initialRoute]}>
+        <App />
+      </MemoryRouter>,
+    )
+  }
+
+  test('should redirect to login page when there is no auth token', () => {
+    vi.spyOn(storage, 'getToken').mockReturnValue(null)
+    renderComponent()
+
+    expect(screen.getByText(/login/i)).toBeInTheDocument()
+  })
+
+  test('should render index page when auth token exists', () => {
+    vi.spyOn(storage, 'getToken').mockReturnValue('fake-token')
+    renderComponent()
+
+    expect(screen.getByText(/hello/i)).toBeInTheDocument()
+  })
+
+  test('should redirect from /login to index page when auth token exists', () => {
+    vi.spyOn(storage, 'getToken').mockReturnValue('fake-token')
+    renderComponent('/login')
+
+    expect(screen.getByText(/hello/i)).toBeInTheDocument()
+  })
+
+  test('should render 404 page if the route does not exist', () => {
+    vi.spyOn(storage, 'getToken').mockReturnValue('fake-token')
+    renderComponent('/non-existent-page')
+
+    expect(screen.getByText(/not found/i)).toBeInTheDocument()
   })
 })
