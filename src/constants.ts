5d37443c1764e4f540d86aa760783a80a13e9814
Serhii Prysiazhnyi <serge.prysiazhnyi@gmail.com>
2024-10-24 12:53:06 +0200

    add types and constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = 'https://playground.tesonet.lt/v1'
+export const TOKEN_URL = `${BASE_URL}/tokens`
+export const SERVERS_URL = `${BASE_URL}/servers`
+export const LOCAL_STORAGE_TOKEN_KEY = 'auth_token'
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+export interface ServerData {
+  name: string
+  distance: number
+}
+
+export interface APITokenResponse {
+  token: string
+}
