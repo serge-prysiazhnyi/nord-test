7a0fddc2a65d2f8e048f08cf28d7c972e327bf2e
Serhii Prysiazhnyi <serge.prysiazhnyi@gmail.com>
2024-10-27 13:41:28 +0100

    refactor App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,45 +1,56 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 
 import App from './App'
-import * as storage from '../src/utils/storage'
+import { AuthState } from '../src/store/auth/authSlice'
+import renderWithStoreProvider from './utils/tests/renderWithStoreProvider'
+import { mockToken } from '../src/mocks/mockTestsData'
 
 describe('App', () => {
   afterEach(() => {
     vi.clearAllMocks()
   })
 
-  const renderComponent = (initialRoute: string = '/') => {
-    render(
+  const renderComponent = (
+    initialRoute: string = '/',
+    preloadedState?: { auth: Partial<AuthState> },
+  ) => {
+    const { store } = renderWithStoreProvider(
       <MemoryRouter initialEntries={[initialRoute]}>
         <App />
       </MemoryRouter>,
+      { preloadedState },
     )
+
+    return {
+      store,
+    }
   }
 
   test('should redirect to login page when there is no auth token', () => {
-    vi.spyOn(storage, 'getToken').mockReturnValue(null)
-    renderComponent()
+    renderComponent('/', { auth: { token: null, isAuthenticated: false } })
 
     expect(screen.getByRole('heading', { name: /login/i })).toBeInTheDocument()
   })
 
   test('should render index page when auth token exists', () => {
-    vi.spyOn(storage, 'getToken').mockReturnValue('fake-token')
-    renderComponent()
+    renderComponent('/', { auth: { token: mockToken, isAuthenticated: true } })
 
     expect(screen.getByRole('heading', { name: /hello/i })).toBeInTheDocument()
   })
 
   test('should redirect from /login to index page when auth token exists', () => {
-    vi.spyOn(storage, 'getToken').mockReturnValue('fake-token')
-    renderComponent('/login')
+    renderComponent('/login', {
+      auth: {
+        token: mockToken,
+        isAuthenticated: true,
+      },
+    })
 
     expect(screen.getByRole('heading', { name: /hello/i })).toBeInTheDocument()
   })
 
   test('should render 404 page if the route does not exist', () => {
-    vi.spyOn(storage, 'getToken').mockReturnValue('fake-token')
     renderComponent('/non-existent-page')
 
     expect(screen.getByText(/not found/i)).toBeInTheDocument()
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
+import { useAppSelector } from '../src/store/hooks'
+import { selectIsAuthenticated } from '../src/store/auth/authSlice'
 import ProtectedRoute from '../src/components/ProtectedRoute/ProtectedRoute'
 import Index from './pages/Index'
 import Login from './pages/Login'
 import Notfound from './pages/Notfound'
-import { getToken } from '../src/utils/storage'
 
 const App: React.FC = () => {
-  const isAuthenticated = Boolean(getToken())
+  const isAuthenticated = useAppSelector(selectIsAuthenticated)
 
   return (
     <Routes>
