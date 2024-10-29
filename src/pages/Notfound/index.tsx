a2de218704186133ad3e55aa46ef1466a5876e1f
Serhii Prysiazhnyi <serge.prysiazhnyi@gmail.com>
2024-10-29 10:24:57 +0100

    improve not-found page

--- src/pages/Notfound/index.tsx
+++ src/pages/Notfound/index.tsx
@@ -1,6 +1,27 @@
 import React, { memo } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Notfound: React.FC = memo(() => <h1>404: Page Not Found</h1>)
+import Layout from '../../components/Layout/Layout'
+import Button from '../../components/Button/Button'
+
+const Notfound: React.FC = memo(() => {
+  const navigate = useNavigate()
+
+  const handleGoBack = () => {
+    navigate(-1)
+  }
+
+  return (
+    <Layout>
+      <h1 className="text-3xl font-bold text-center">404: Page Not Found</h1>
+      <div className="flex justify-center items-center">
+        <Button className="mt-3 mx-auto" onClick={handleGoBack} type="button">
+          Go Back
+        </Button>
+      </div>
+    </Layout>
+  )
+})
 
 Notfound.displayName = 'Notfound'
 
