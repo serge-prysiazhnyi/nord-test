b8f7d788f542ecd408a8b12368174030b264a112
Serhii Prysiazhnyi <serge.prysiazhnyi@gmail.com>
2024-10-29 13:41:40 +0100

    add bundle visualizer and manual chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    visualizer({
+      filename: 'bundle-report.html',
+      open: true,
+      template: 'treemap',
+    }),
     legacy({
       targets: ['defaults', 'not IE 10'],
       additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux'],
+          router: ['react-router-dom'],
+        },
+      },
+    },
+  },
 })
